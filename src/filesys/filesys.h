2722559622763e1b5b8c2782f1d4926542f452d3
Taeyoon Kim <tykimseoul@gmail.com>
2019-12-03 20:03:43 +0900

    refactor: code formatting

--- src/filesys/filesys.h
+++ src/filesys/filesys.h
@@ -11,10 +11,14 @@
 /* Block device that contains the file system. */
 struct block *fs_device;
 
-void filesys_init (bool format);
-void filesys_done (void);
-bool filesys_create (const char *name, off_t initial_size);
-struct file *filesys_open (const char *name);
-bool filesys_remove (const char *name);
+void filesys_init(bool format);
+
+void filesys_done(void);
+
+bool filesys_create(const char *name, off_t initial_size);
+
+struct file *filesys_open(const char *name);
+
+bool filesys_remove(const char *name);
 
 #endif /* filesys/filesys.h */
